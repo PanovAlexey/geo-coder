f796bcec5974b0e87dfe71c1903bc1a93036924e
Panov Alexey <alexeykapanov@gmail.com>
2020-08-21 20:49:44 +0300

    Create abstract class with common methods for geolocation provider.

---
+++ src/Providers/AbstractProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoCoder\Providers;
+
+abstract class AbstractProvider
+{
+    public function getName(): string
+    {
+        return get_class($this);
+    }
+
+    protected function httpStatusCodeValidation(int $httpStatusCode): void
+    {
+        if ($httpStatusCode > 299 && $httpStatusCode < 399) {
+            throw new RedirectionServerResponseException('HTTP status code=' . $httpStatusCode);
+        }
+
+        if ($httpStatusCode > 399 && $httpStatusCode < 499) {
+            throw new InvalidRequestException('HTTP status code=' . $httpStatusCode);
+        }
+
+        if ($httpStatusCode > 499 && $httpStatusCode < 599) {
+            throw new InvalidServerResponseException('HTTP status code=' . $httpStatusCode);
+        }
+    }
+
+    protected function isJson(string $string): bool
+    {
+        json_decode($string);
+
+        return (json_last_error() === JSON_ERROR_NONE);
+    }
+}
