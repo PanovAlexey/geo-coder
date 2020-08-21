8a6730d30bd7cf8f8cd63c344d19d0ed82cba8e4
Panov Alexey <alexeykapanov@gmail.com>
2020-08-21 20:52:32 +0300

    Create main application class.

---
+++ src/Geocoder.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoCoder;
+
+use CodeblogPro\GeoCoder\Coordinates\Coordinates;
+use CodeblogPro\GeoCoder\Location\LocationInterface;
+use CodeblogPro\GeoCoder\Providers\ProviderInterface;
+
+class Geocoder
+{
+    private ProviderInterface $provider;
+    private string $locale;
+
+    public function __construct(ProviderInterface $provider, string $locale = '')
+    {
+        $this->provider = $provider;
+        $this->locale = $locale;
+    }
+
+    public function getLocationByCoordinates($latitude, $longitude): LocationInterface
+    {
+        $coordinates = new Coordinates($latitude, $longitude);
+
+        return $this->provider->getLocationByCoordinates($coordinates, $this->locale);
+    }
+
+    public function getLocationByAddress(string $address): LocationInterface
+    {
+        return $this->provider->getLocationByAddress($address);
+    }
+
+    public function getProviderName(): string
+    {
+        return $this->provider->getName();
+    }
+}
