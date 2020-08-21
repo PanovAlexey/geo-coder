a9c39cb5fb5c1a2b2a080ab05b06c3a458dcd09c
Panov Alexey <alexeykapanov@gmail.com>
2020-08-21 20:50:40 +0300

    Create custom exceptions.

---
+++ src/Exception/GeoCoderException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoCoder\Exception;
+
+/**
+ * This class represents a generic error.
+ */
+class GeoCoderException extends \Exception
+{
+}
---
+++ src/Exception/InvalidRequestException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeblogPro\GeoCoder\Exception;
+
+class InvalidRequestException extends GeoCoderException
+{
+
+}
